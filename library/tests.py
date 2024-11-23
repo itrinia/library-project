9e52bfac98da67bb9203858701afbd99249ef088
ileene santoso <ileenesantoso@gmail.com>
2024-11-23 16:30:02 +0700

    update test

--- library/tests.py
+++ library/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from .models import Book
 
-class BookCRUDTests(TestCase):
-    def test_create_book(self):
-        book = Book.objects.create(title="Test Book", author="Test Author")
-        self.assertEqual(Book.objects.count(), 1)
-        self.assertEqual(book.title, "Test Book")
+class BookModelTest(TestCase):
+    def test_string_representation(self):
+        book = Book(title="A Sample Book")
+        self.assertEqual(str(book), book.title)
